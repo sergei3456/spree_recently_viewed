0bf40d2ad3512d4fb2159dcad97a2900720c50ce
Сергей Сечин <sechins01@yandex.ru>
2020-11-02 18:49:39 +0300

    Add new id override for view

--- app/overrides/spree/products/show/add_recently_viewed_products_to_products_show.rb
+++ app/overrides/spree/products/show/add_recently_viewed_products_to_products_show.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'add_recently_viewed_products_to_products_show',
-  insert: "#recently-viewed",
+  replace: "#recently-viewed",
   partial: 'spree/shared/add_recently_viewed_products'
 )
